781992a7e2395ea75b702c02b670d1003101a2d5
iameph <iameph@yandex.ru>
2014-02-14 15:18:10 +0600

    add (ExerciseData) Gender, UserData

---
+++ ExerciseData/UserData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRM_Track_Merger.ExerciseData {
+    class UserData {
+        public double? Weight { get; set; }
+        public int? MaxHR { get; set; }
+        public int? RestHR { get; set; }
+        public double? VO2Max { get; set; }
+        public int? Age { get; set; }
+        public Gender? Gender;
+    }
+}
