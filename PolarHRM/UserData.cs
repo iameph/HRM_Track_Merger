cc2af8476694ae514d529ea129bdea155a43c83c
iameph <iameph@yandex.ru>
2014-02-14 15:23:32 +0600

    fix (PolarHRM) UserData properties -> fields

--- PolarHRM/UserData.cs
+++ PolarHRM/UserData.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace HRM_Track_Merger.PolarHRM {
     class UserData : ICloneable {
-        public double? Weight { get; set; }
-        public int? MaxHR { get; set; }
-        public int? RestHR { get; set; }
-        public double? VO2Max { get; set; }
-        public int? Age { get; set; }
+        public double? Weight;
+        public int? MaxHR;
+        public int? RestHR;
+        public double? VO2Max;
+        public int? Age;
         public object Clone() {
             return new UserData() {
                 Weight = this.Weight,
