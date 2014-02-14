2b87b92bf40a190a9d063dd341733c130d189aa1
iameph <iameph@yandex.ru>
2014-02-14 14:44:04 +0600

    add (ExerciseData) DataPoint interpolate methods

--- ExerciseData/DataPoint.cs
+++ ExerciseData/DataPoint.cs
@@ -25,5 +25,32 @@ namespace HRM_Track_Merger.ExerciseData {
         public object Clone() {
             return MemberwiseClone();
         }
+        public static DataPoint Interpolate(DataPoint dataPoint1, DataPoint dataPoint2, DateTime time) {
+            double x1 = 0;
+            double x2 = (dataPoint2.Time - dataPoint1.Time).TotalSeconds;
+            double x = (time - dataPoint1.Time).TotalSeconds;
+            return new DataPoint() {
+                AirPressure = Interpolate(x1, x2, dataPoint1.AirPressure, dataPoint2.AirPressure, x),
+                Altitude = Interpolate(x1, x2, dataPoint1.Altitude, dataPoint2.Altitude, x),
+                Cadence = Interpolate(x1, x2, dataPoint1.Cadence, dataPoint2.Cadence, x),
+                Distance = Interpolate(x1, x2, dataPoint1.Distance, dataPoint2.Distance, x),
+                HeartRate = Interpolate(x1, x2, dataPoint1.HeartRate, dataPoint2.HeartRate, x),
+                Latitude = Interpolate(x1, x2, dataPoint1.Latitude, dataPoint2.Latitude, x),
+                Longitude = Interpolate(x1, x2, dataPoint1.Longitude, dataPoint2.Longitude, x),
+                Power = Interpolate(x1, x2, dataPoint1.Power, dataPoint2.Power, x),
+                PowerBalance = Interpolate(x1, x2, dataPoint1.PowerBalance, dataPoint2.PowerBalance, x),
+                Speed = Interpolate(x1, x2, dataPoint1.Speed, dataPoint2.Speed, x),
+                Time = time
+            };
+        }
+        public static double Interpolate(double x1, double x2, double y1, double y2, double x) {
+            if (x <= x1) {
+                return y1;
+            }
+            if (x >= x2) {
+                return y2;
+            }
+            return (x - x1) * (y2 - y1) / (x2 - x1) + y1;
+        }
     }
 }
