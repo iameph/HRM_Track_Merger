cd602255526a7a888c51a2c40978fe21c6173a4f
iameph <iameph@yandex.ru>
2014-02-14 15:31:00 +0600

    add (Program) rewrite CommandLineArguments

--- CommandLineArguments.cs
+++ CommandLineArguments.cs
@@ -4,84 +4,73 @@ using System.IO;
 
 namespace HRM_Track_Merger {
     class CommandLineArguments {
+        private List<string> hrmExtensions= new List<string>(new string[]{".hrm",".xml"});
+        private List<string> gpsExtensions = new List<string>(new string[] { ".gpx" });
+        private Dictionary<string, string> options = new Dictionary<string,string>();
         protected CommandLineArguments(string[] args) {
-            if (args.Length < 2) {
-                throw new InvalidArgumentsException("Wrong arguments");
+            if (args.Length == 0) {
+                throw new InvalidArgumentsException("No arguments");
             }
-            if (isFileName(args[0]) && isFileName(args[1])) {
-                if (Path.GetExtension(args[0]).Equals(".gpx")) {
-                    GetTimeFromFile(args[1]);
-                    FileName = args[0];
+            foreach (var arg in args) {
+                if (isFileName(arg)) {
+                    if (hrmExtensions.Contains(Path.GetExtension(arg))) {
+                        if (HRMFileName != null)
+                            throw new InvalidArgumentsException(String.Format(
+                                "Two HRM files in input: {0} and {1}. Please check data.", HRMFileName, arg));
+                        HRMFileName = arg;
+                    }
+                    else if (gpsExtensions.Contains(Path.GetExtension(arg))) {
+                        if (GPSFileName != null)
+                            throw new InvalidArgumentsException(String.Format(
+                                "Two GPS files in input: {0} and {1}. Please check data.", GPSFileName, arg));
+                        GPSFileName = arg;
+                    }
+                    else {
+                        throw new InvalidArgumentsException(String.Format(
+                                "Unknown type of file {0}. Please check data.", arg));
+                    }
+                }
+                else if (IsOption(arg)) {
+                    AddOption(arg);
                 }
                 else {
-                    GetTimeFromFile(args[0]);
-                    FileName = args[1];
-                }
-                if (args.Length > 2) {
-                    Offset = SecondsToTimeSpan(args[2]);
+                    throw new InvalidArgumentsException(String.Format("Wrong argument: {0}", arg));
                 }
             }
-            else if (args.Length < 5) {
-                throw new InvalidArgumentsException("Wrong arguments");
+        }
+
+        private void AddOption(string arg) {
+            var m = System.Text.RegularExpressions.Regex.Match(arg, @"/([0-9a-zA-Z]+):(.+)");
+            var key = m.Groups[1].Value;
+            var val = m.Groups[2].Value;
+            val = val.Trim(new char[] { ' ', '\t', '"' });
+            if (options.ContainsKey(key)) {
+                throw new InvalidArgumentsException("Two option arguments with same name: " + key + ". Please check data.");
             }
-            else {
-                FileName = args[0];
-                Offset = SecondsToTimeSpan(args[1]);
-                StartTime = DateTime.Parse(args[2]);
-                Duration = TimeSpan.Parse(args[3]);
-                Step = SecondsToTimeSpan(args[4]);
-            }
-            Duration = Duration.Subtract(TimeSpan.FromMilliseconds(Duration.Milliseconds));
-            StartTime = StartTime.Subtract(TimeSpan.FromMilliseconds(StartTime.Millisecond));
+            options.Add(key, val);
+        }
+
+        internal bool IsOption(string arg) {
+            return System.Text.RegularExpressions.Regex.Match(arg,@"/[0-9a-zA-Z]+:.+").Success;
         }
         private TimeSpan SecondsToTimeSpan(string p) {
             return new TimeSpan(0, 0, Int32.Parse(p));
         }
-
-        private void GetTimeFromFile(string fileName) {
-            var Parser = HRMParser.GetParser(fileName);
-            var paramsSection = Parser.GetSection("Params");
-            var pairs = new Dictionary<string, string>();
-            foreach (var str in paramsSection) {
-                var pair = str.Split(new char[] { '=' }, 2);
-                if (pair.Length > 1) {
-                    pairs.Add(pair[0], pair[1]);
-                }
-            }
-            try {
-                StartTime = GetDateFromString(pairs["Date"]) + TimeSpan.Parse(pairs["StartTime"]);
-                Duration = TimeSpan.Parse(pairs["Length"]);
-                Step = SecondsToTimeSpan(pairs["Interval"]);
-            }
-            catch (Exception e) {
-                throw new InvalidFileFormatException(e.Message);
-            }
-        }
-
         private bool isFileName(string name) {
             return File.Exists(name);
         }
         #region Properties
-        public string FileName { get; set; }
-        public TimeSpan Offset { get; set; }
-        public DateTime StartTime { get; set; }
-        public TimeSpan Duration { get; set; }
-        public TimeSpan Step { get; set; }
+        
+        public string HRMFileName { get; set; }
+        public string GPSFileName { get; set; }
         #endregion
-        public void FixTimeForTCXCreator() {
-            Offset = Offset.Add(Step);
-            StartTime = StartTime.Add(Step);
-        }
+        
         public static CommandLineArguments Parse(string[] args) {
-
             return new CommandLineArguments(args);
         }
-        public void SetDateForStartTime(DateTime date) {
-            StartTime = new DateTime(date.Year, date.Month, date.Day, StartTime.Hour, StartTime.Minute, StartTime.Second);
+        public Dictionary<string, string> GetOptions() {
+            return new Dictionary<string, string>(options);
         }
-        private DateTime GetDateFromString(string str) {
-            return new DateTime(Int32.Parse(str.Substring(0, 4)), Int32.Parse(str.Substring(4, 2)),
-                                        Int32.Parse(str.Substring(6, 2)));
-        }
+        
     }
 }
