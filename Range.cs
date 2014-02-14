f52a246d31ea12001da22bd5d453192bcb6d2340
iameph <iameph@yandex.ru>
2014-02-14 15:55:08 +0600

    refactor : move Range from ExerciseData to main namespace

--- Range.cs
+++ Range.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HRM_Track_Merger.ExerciseData {
+namespace HRM_Track_Merger {
     struct Range<T> {
         public T Min;
         public T Max;
