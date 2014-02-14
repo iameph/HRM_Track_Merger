52ac36dce1543169e5cebf59bf325d337b8124be
iameph <iameph@yandex.ru>
2014-02-14 14:40:00 +0600

    fix (PolarHRM) datapoint: make Altitude as double, add Distance

--- PolarHRM/HRDataPoint.cs
+++ PolarHRM/HRDataPoint.cs
@@ -9,10 +9,11 @@ namespace HRM_Track_Merger.PolarHRM {
         public int HeartRate;
         public double Speed;
         public int Cadence;
-        public int Altitude;
+        public double Altitude;
         public int Power;
         public int PowerBalance;
         public int AirPressure;
         public DateTime Time;
+        public double Distance;
     }
 }
