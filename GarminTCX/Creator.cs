b52ee2ea89682c1a1ba0bcfd0fc3989f0258d64a
iameph <iameph@yandex.ru>
2014-02-14 16:22:11 +0600

    fix (GarminTCX) Creator.GenerateXML(): Version!=null check

--- GarminTCX/Creator.cs
+++ GarminTCX/Creator.cs
@@ -13,17 +13,19 @@ namespace HRM_Track_Merger.GarminTCX {
             var attr = doc.CreateAttribute("xsi","type","http://www.w3.org/2001/XMLSchema-instance");
             attr.Value = "Device_t";
             elem.SetAttributeNode(attr);
-            elem.AppendChild(doc.CreateElement("Name", xmlNS)).InnerXml = Name;
+            elem.AppendChild(doc.CreateElement("Name", xmlNS)).InnerXml = Name!=null?Name:"";
             elem.AppendChild(doc.CreateElement("UnitId", xmlNS)).InnerXml = UnitID.ToString();
             elem.AppendChild(doc.CreateElement("ProductID", xmlNS)).InnerXml = ProductID.ToString();
-            elem.AppendChild(doc.CreateElement("Version", xmlNS));
-            elem["Version"].AppendChild(doc.CreateElement("VersionMajor", xmlNS)).InnerXml = Version[0].ToString();
-            elem["Version"].AppendChild(doc.CreateElement("VersionMinor", xmlNS)).InnerXml = Version[1].ToString();
-            if (Version.Length > 2) {
-                elem["Version"].AppendChild(doc.CreateElement("BuildMajor", xmlNS)).InnerXml = Version[2].ToString();
-            }
-            if (Version.Length > 3) {
-                elem["Version"].AppendChild(doc.CreateElement("BuildMinor", xmlNS)).InnerXml = Version[3].ToString();
+            if (Version != null) {
+                elem.AppendChild(doc.CreateElement("Version", xmlNS));
+                elem["Version"].AppendChild(doc.CreateElement("VersionMajor", xmlNS)).InnerXml = Version[0].ToString();
+                elem["Version"].AppendChild(doc.CreateElement("VersionMinor", xmlNS)).InnerXml = Version[1].ToString();
+                if (Version.Length > 2) {
+                    elem["Version"].AppendChild(doc.CreateElement("BuildMajor", xmlNS)).InnerXml = Version[2].ToString();
+                }
+                if (Version.Length > 3) {
+                    elem["Version"].AppendChild(doc.CreateElement("BuildMinor", xmlNS)).InnerXml = Version[3].ToString();
+                }
             }
             return elem;
         }
