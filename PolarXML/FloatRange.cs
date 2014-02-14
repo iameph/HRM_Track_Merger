1fdaba41767901cb35f41c1e3fb870508c6e1681
iameph <iameph@yandex.ru>
2014-02-14 15:57:06 +0600

    add (PolarXML) FloatRange, ShortRange reference classes

---
+++ PolarXML/FloatRange.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace HRM_Track_Merger.PolarXML {
+    class FloatRange {
+        public FloatRange() { }
+        public FloatRange(XmlElement elem) {
+            ParseXmlElement(elem);
+        }
+        private void ParseXmlElement(XmlElement elem) {
+            if (elem["minimum"] != null) {
+                Minimum = Double.Parse(elem["minimum"].InnerXml);
+            }
+            if (elem["average"] != null) {
+                Average = Double.Parse(elem["average"].InnerXml);
+            }
+            if (elem["maximum"] != null) {
+                Maximum = Double.Parse(elem["maximum"].InnerXml);
+            }
+        }
+        public static FloatRange Parse(XmlElement elem) {
+            return new FloatRange(elem);
+        }
+        public double? Minimum;
+        public double? Maximum;
+        public double? Average;
+    }
+}
