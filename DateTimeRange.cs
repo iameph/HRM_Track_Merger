137aeb1201aa62899c9bebf24c52178720b7da82
iameph <iameph@yandex.ru>
2014-02-14 14:35:57 +0600

    add DateTimeRange

---
+++ DateTimeRange.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRM_Track_Merger {
+    class DateTimeRange {
+        public DateTimeRange(DateTime start, DateTime end){
+            Start = start;
+            End = end;
+            Duration = end - start;
+        }
+        public DateTimeRange(DateTime start, TimeSpan duration) {
+            Start = start;
+            Duration = duration;
+            End = Start + Duration;
+        }
+        public DateTime Start { get; private set; }
+        public DateTime End { get; private set; }
+        public TimeSpan Duration { get; private set; }
+    }
+}
