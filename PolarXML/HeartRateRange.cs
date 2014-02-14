48d0ed0f8513d9dfd4351e3ba325084f3eaedd1b
iameph <iameph@yandex.ru>
2014-02-14 16:11:11 +0600

    fix (PolarXML) : HeartRateRange: ShortRange

--- PolarXML/HeartRateRange.cs
+++ PolarXML/HeartRateRange.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using System.Xml;
 
 namespace HRM_Track_Merger.PolarXML {
-    class HeartRateRange : ICloneable {
+    class HeartRateRange : ShortRange,ICloneable {
         public HeartRateRange() { }
         public HeartRateRange(XmlElement elem) {
             ParseXmlElement(elem);
@@ -34,17 +34,17 @@ namespace HRM_Track_Merger.PolarXML {
                 }
             }
         }
-        public static HeartRateRange Parse(XmlElement elem) {
+        public static new HeartRateRange Parse(XmlElement elem) {
             return new HeartRateRange(elem);
         }
         //<xs:element name="resting" minOccurs="0">
         public byte? Restring;
         //<xs:element name="minimum" minOccurs="0">
-        public byte? Minimum;
+        //public byte? Minimum;
         //<xs:element name="average" minOccurs="0">
-        public byte? Average;
+        //public byte? Average;
         //<xs:element name="maximum" minOccurs="0">
-        public byte? Maximum;
+        //public byte? Maximum;
         //<xs:element name="ending" minOccurs="0">
         public byte? Ending;
 
