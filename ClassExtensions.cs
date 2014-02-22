2c9e36f8b7b602d3bc0a20502ce89f74cb232a44
iameph <iameph@yandex.ru>
2014-02-22 13:42:24 +0600

    add XmlElement.AppendNotNullChild() extension
    extension method appends child only if it is not null, does nothing otherwise

---
+++ ClassExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HRM_Track_Merger {
+    public static class ClassExtensions {
+        public static System.Xml.XmlNode AppendNotNullChild(this System.Xml.XmlElement parent, System.Xml.XmlNode child) {
+            if (child == null)
+                return null;
+            parent.AppendChild(child);
+            return child;
+        }
+    }
+}
